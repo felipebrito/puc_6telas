041fbd0a9ee1d8f128d7e6c3ce2db645b42fc33e
Felipe Brito <brito@MacBook-Pro-de-Felipe-3.local>
2026-05-05 19:29:00 -0300

    Adiciona porta e http ao local ip

--- ecosystem.mini1.config.js
+++ ecosystem.mini1.config.js
@@ -15,7 +15,7 @@ module.exports = {
       script: "slave.js",
       watch: false,
       env: {
-        MASTER_IP: "127.0.0.1",
+        MASTER_IP: "http://127.0.0.1:3000",
         VIDEO_1_PATH: path.join(__dirname, "videos", "screen_1.mp4"),
         VIDEO_2_PATH: path.join(__dirname, "videos", "screen_2.mp4"),
         VIDEO_3_PATH: path.join(__dirname, "videos", "screen_3.mp4"),
